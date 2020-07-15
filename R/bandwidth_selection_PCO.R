b575699aa3bd07676c832d69299a74d6815a4cbf
Leon Patzig <leon@regelt.es>
2020-07-15 09:53:54 +0200

    outsource definition of common kernel functions

--- R/bandwidth_selection_PCO.R
+++ R/bandwidth_selection_PCO.R
@@ -1,25 +1,4 @@
-kernels <- c(
-             gaussian = function(u) {
-                 1 / (sqrt(2*pi)) * exp(-u^2 / 2)
-             },
-             rectangular = function(u) {
-                 1/2 * (abs(u) <= 1)
-             },
-             triangular = function(u) {
-                 (1 - abs(u)) * (abs(u) <= 1)
-             },
-             epanechnikov = function(u) {
-                 3/4 * (1 - u^2) * (abs(u) <= 1)
-             },
-             biweight = function(u) {
-                 15/16 * (1 - u^2)^2 * (abs(u) <= 1)
-             },
-             silverman = function(u) {
-                 1/2 * exp(-abs(u)/sqrt(2)) *
-                     sin(abs(u)/sqrt(2) + pi/4)
-             }
-)
-kernels$parabolic <- kernels$epanechnikov
+source('kernels.R')
 
 K_h <- function(h, t, test) {
     test$K(t / h) / h
