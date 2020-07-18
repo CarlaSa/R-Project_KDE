c37ef47e5e17c956e9a97f2e155820732c26d42b
Leon <leon@regelt.es>
2020-07-18 23:08:40 +0200

    rename parameters → improve explanatory power

--- R/rejection_sample.R
+++ R/rejection_sample.R
@@ -6,25 +6,25 @@ source('kernels.R')
 #' function using the rejection sampling method.
 #'
 #' @param n_obs An integer vector of length 1. The number of observations.
-#' @param Kernel A probability density function to generate observations from.
-#' @param Y A helper distribution function.
-#' @param g A real function. The probability density function of the Y
+#' @param f A probability density function to simulate observations from.
+#' @param helper_distribution A helper distribution function.
+#' @param helper_density A real function. The probability density function of the Y
 #' distribution.
 #' @param n_iter An integer vector of length 1. A calibrating constant. The
 #' algorithm will take an average of n_iter iterations to obtain a sample.
 #' @return A double vector of length n_obs. The observations.
 rejection_sample <- function(n_obs,
-                  Kernel,
-                  Y = function() rnorm(1, 0, 1),
-                  g = kernels$gaussian,
-                  n_iter = 10
-) {
+                             f,
+                             helper_distribution = function() rnorm(1, 0, 1),
+                             helper_density = kernels$gaussian,
+                             n_iter = 10
+                            ) {
     sapply(seq(n_obs), function(i) {
-               while (TRUE) {
-                   y <- Y()
-                   u <- runif(1, 0, 1)
-                   if (u < Kernel(y) / n_iter / g(y))
-                       return(y)
-               }
+        while (TRUE) {
+            y <- helper_distribution()
+            u <- runif(1, 0, 1)
+            if (u < f(y) / n_iter / helper_density(y))
+                return(y)
+            }
     })
 }
