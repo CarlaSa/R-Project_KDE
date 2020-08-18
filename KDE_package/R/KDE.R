7d54cde88ff78310827e877a1d411c7009b07a60
Leon <leon@regelt.es>
2020-08-18 17:10:12 +0200

    get_KDE: introduce the option to provide the ›Kernel‹ parameter as the name of a Kernel from the ›kernels‹ list

--- KDE_package/R/KDE.R
+++ KDE_package/R/KDE.R
@@ -29,6 +29,9 @@ kde <- function(x, h, Kernel, data) {
 #' @return A double vector of length 1.
 #' @export
 get_kde <- function(h, Kernel, data) {
+    stopifnot('Kernel needs to be a Kernel object, a function or the name of a Kernel in `kernels`.' = is_Kernel(Kernel) || is.function(Kernel) || (Kernel %in% names(kernels)))
+    if(Kernel %in% names(kernels))
+        Kernel <- kernels[Kernel]
     function(x) {
         sapply(x, kde, h, Kernel, data)
     }
