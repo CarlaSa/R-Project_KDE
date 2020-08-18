f733bd6a7da0c150d2721d61804a44479ca66b02
Leon <leon@regelt.es>
2020-08-18 14:36:01 +0200

    remove the obsoleted out-of-package code directory

--- R/tools.R
+++
@@ -1,9 +0,0 @@
-#' Vectorise a single-parameter function using sapply.
-#'
-#' @param f A single-parameter function.
-#' @return A vectorised single-parameter function.
-sapplify <- function(f) {
-    function(a) {
-        sapply(a, f)
-    }
-}
