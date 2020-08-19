19f84329e6f6b20b22791348fbf0ee8b96b7b02a
Charlotte May Boys <charlottemayboys@Charlottes-MacBook-Air.local>
2020-08-19 15:12:46 +0200

    export pdf functions for use in vignette and shiny app

--- KDE_package/R/pdf.R
+++ KDE_package/R/pdf.R
@@ -5,6 +5,7 @@
 #' @param x0 A double vector of length 1. The location parameter.
 #' @param gamma A double vector of length 1. The scale parameter.
 #' @return A vectorised function. A Cauchy distribution function.
+#' @export
 get_cauchy <- function(x0 = 0, gamma = 0.5) {
     function(x)
         1/(pi * gamma * (1 + ((x - x0) / gamma)^2))
@@ -15,6 +16,7 @@ get_cauchy <- function(x0 = 0, gamma = 0.5) {
 #' @param a A double vector of length 1. A location parameter.
 #' @param b A double vector of length 1. A location parameter.
 #' @return A vectorised function. A uniform distribution function.
+#' @export
 get_uniform <- function(a = -1, b = 1) {
     function(x)
         abs(1/(b-a)) * (a <= x & x <= b)
@@ -25,6 +27,7 @@ get_uniform <- function(a = -1, b = 1) {
 #' @param lambda A double vector of length 1.
 #' The rate (inverse scale) parameter.
 #' @return A vectorised function. An exponential distribution function.
+#' @export
 get_exponential <- function(lambda = 1) {
     function(x)
         lambda * exp(-lambda * x) * (x >= 0)
@@ -35,6 +38,7 @@ get_exponential <- function(lambda = 1) {
 #' @param mu A double vector of length 1. The mean.
 #' @param sigma A double vector of length 1. The standard deviation.
 #' @return A vectorised function. A normal distribution function.
+#' @export
 get_normal <- function(mu = 0, sigma = 1) {
     function(x)
         1/(sigma * sqrt(2 * pi)) * exp(-1/2 * ((x - mu)/sigma)^2)
@@ -45,6 +49,7 @@ get_normal <- function(mu = 0, sigma = 1) {
 #' @param mu A double vector of length 1. The mean.
 #' @param b A double vector of length 1. The scale parameter.
 #' @return A vectorised function. A Laplace distribution function.
+#' @export
 get_laplace <- function(mu = 0, b = 1) {
     function(x) {
         1/(2*b) * exp(-abs(x - mu)/b)
@@ -55,11 +60,13 @@ get_laplace <- function(mu = 0, b = 1) {
 #'
 #' @param str A character vector of lengh 1. A string containing a function expression using a numeric parameter named x. Must be a valid function body. The resulting function must be vectorised.
 #' @return A vectorised function. A custom distribution function.
+#' @export
 get_custom <- function(str = 'abs(x)/4 * (abs(x) <= 2)') {
     function(x) eval(rlang::parse_expr(str))
 }
 
 #' Some probability density functions.
+#' @export
 pdfs <- list(
     Cauchy = get_cauchy(0, 0.5),
     Uniform = get_uniform(0, 1),
@@ -73,6 +80,7 @@ pdfs <- list(
 )
 
 #' All probability density functions available in one list.
+#' @export
 pdf_factories <- list(
     Cauchy = get_cauchy,
     uniform = get_uniform,
