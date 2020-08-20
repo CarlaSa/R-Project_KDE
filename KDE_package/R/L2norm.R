1fda6fda5305484bf823c6ae670ff78e1e7cf898
Leon <leon@regelt.es>
2020-08-21 00:35:29 +0200

    switching to hcubature as pcubature would cause corrupt results when using PCO with the triangular kernel

--- KDE_package/R/L2norm.R
+++ KDE_package/R/L2norm.R
@@ -9,7 +9,7 @@ L2norm_squared <- function(f, maxEval) {
     if(is_Kernel(f))
         return(attr(f, 'L2norm_squared'))
     #integrate(function(u) f(u)^2, lower = -Inf, upper = Inf)$value
-    cubature::cubintegrate(function(u) f(u)^2, lower = -Inf, upper = Inf, method = "pcubature", maxEval=maxEval)$integral
+    cubature::cubintegrate(function(u) f(u)^2, lower = -Inf, upper = Inf, method = "hcubature", maxEval=maxEval)$integral
 }
 
 #' Calculate L2-norm of a real function.
